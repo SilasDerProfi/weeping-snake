0265ab42106047d14557d54823b57d0b1ee0ee70
SilasDerProfi <silas@schnurr.one>
2021-02-04 12:47:28 +0100

    Make PlayerDirection.Length Lazy Loaded and a Getter instead of a method

--- src/WeepingSnake.Game/Player/PlayerDirection.cs
+++ src/WeepingSnake.Game/Player/PlayerDirection.cs
@@ -6,17 +6,19 @@ namespace WeepingSnake.Game.Player
     public struct PlayerDirection
     {
         private readonly Vector2 _direction;
+        private readonly float _length;
 
         public PlayerDirection(float directionX, float directionY) : this()
         {
             _direction.X = directionX;
             _direction.Y = directionY;
+            _length = _direction.Length();
         }
 
         public double X => _direction.X;
         public double Y => _direction.Y;
 
-        public float Length() => _direction.Length();
+        public float Length => _length;
         
         public static PlayerDirection RandomPlayerDirection()
         {
@@ -35,7 +37,7 @@ namespace WeepingSnake.Game.Player
                    _direction.Equals(direction._direction);
         }
 
-        public override int GetHashCode() => HashCode.Combine(_direction, 23);
+        public override int GetHashCode() => HashCode.Combine(_direction, Length);
 
         public static bool operator ==(PlayerDirection left, PlayerDirection right) => left.Equals(right);
 
