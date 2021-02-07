906b508e5abd8fe6395e1578ad4eed1c7a69c1eb
SilasDerProfi <silas@schnurr.one>
2021-02-07 20:29:28 +0100

    Add first implementation of Bresenham

--- src/WeepingSnake.Game/Game/Game.Board.cs
+++ src/WeepingSnake.Game/Game/Game.Board.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Numerics;
 using WeepingSnake.Game.Geometry;
@@ -30,8 +31,61 @@ namespace WeepingSnake.Game
                 return new PlayerOrientation(position, direction);
             }
 
-            // Random start position with a padding
-            private GameCoordinate CalculateRandomStarCoordinates() => throw new NotImplementedException();
+            private GameCoordinate CalculateRandomStarCoordinates()
+            {
+                throw new NotImplementedException();
+                var possiblePosition = new HashSet<GameCoordinate>();
+
+                for (int x = 1; x < _dimensions.Width - 1; x++)
+                    for (int y = 1; y < _dimensions.Height - 1; y++)
+                        possiblePosition.Add(new GameCoordinate(x, y, (uint)_paths.Count));
+
+                foreach(var vector in _paths[^1])
+                {
+                    // get affected positions from vector
+                    _ = CalculatePointsOnLine(0, 0, 0, 0);
+
+                    // add a padding
+                        // around all found affected positions
+
+                    // remove the positions from the possible start positions
+                }
+
+                // retrun a remaining start position (random or the one in the largest cluster nearest to the center)
+            }
+        }
+
+        /// <summary>
+        /// via Bresenham's line algorithm
+        /// </summary>
+        private static IEnumerable<(int, int)> CalculatePointsOnLine(int x0, int y0, int x1, int y1)
+        {
+            if (Math.Abs(y1 - y0) > Math.Abs(x1 - x0))
+                return CalculatePointsOnLine(y0, x0, y1, x1).Select(i => (i.Item2, i.Item1));
+            else if (x0 > x1)
+                return CalculatePointsOnLine(x1, y1, x0, y0);
+            else
+                return Bresenham(x0, y0, x1, y1);
+
+            static IEnumerable<(int, int)> Bresenham(int x0, int y0, int x1, int y1)
+            {
+                int dx = x1 - x0;
+                int dy = Math.Abs(y1 - y0);
+                int error = dx / 2;
+                int ystep = (y0 < y1) ? 1 : -1;
+                int y = y0;
+
+                for (int x = x0; x <= x1; x++)
+                {
+                    yield return (x, y);
+                    error -= dy;
+                    if (error < 0)
+                    {
+                        y += ystep;
+                        error += dx;
+                    }
+                }
+            }
         }
     }
 }
