b0225d49109e490900883eec1ef3c1daa96d000d
SilasDerProfi <silas@schnurr.one>
2021-02-11 21:21:25 +0100

    Add Bresenham steep and flat line unit test

--- src/tests/WeepingSnake.Game.Tests/Geometry/CoordinateSystemTests.cs
+++ src/tests/WeepingSnake.Game.Tests/Geometry/CoordinateSystemTests.cs
@@ -104,15 +104,65 @@ namespace WeepingSnake.Game.Tests.Geometry
         [Fact]
         public void CalculatePointsOnFlatLineTest()
         {
-            var coordinateSystem = new MockCoordinateSystem();
-            //throw new NotImplementedException();
+            var actualA = MockCoordinateSystem.CalculatePointsOnLine(-2, -1, 2, 1);
+            var expectedA = new List<(int, int)>()
+            {
+                (-2,-1), (-1,-1), (0,0), (1,0), (2,1)
+            };
+            Assert.Equal(expectedA, actualA);
+
+            var actualB = MockCoordinateSystem.CalculatePointsOnLine(4, 1, -4, -1);
+            var expectedB = new List<(int, int)>()
+            {
+                 (-4,-1), (-3,-1), (-2,-1), (-1,0), (0,0), (1,0), (2,0), (3,1), (4,1)
+            };
+            Assert.Equal(expectedB, actualB);
+
+            var actualC = MockCoordinateSystem.CalculatePointsOnLine(-3, 1, 4, -1);
+            var expectedC = new List<(int, int)>()
+            {
+               (-3,1), (-2,1), (-1,0), (0,0), (1,0), (2,0), (3,-1), (4,-1)
+            };
+            Assert.Equal(expectedC, actualC);
+
+            var actualD = MockCoordinateSystem.CalculatePointsOnLine(4, -1, -3, 1);
+            var expectedD = new List<(int, int)>()
+            {
+                (-3,1), (-2,1), (-1,0), (0,0), (1,0), (2,0), (3,-1), (4,-1)
+            };
+            Assert.Equal(expectedD, actualD);
         }
 
         [Fact]
         public void CalculatePointsOnSteepLineTest()
         {
-            var coordinateSystem = new MockCoordinateSystem();
-            //throw new NotImplementedException();
+            var actualA = MockCoordinateSystem.CalculatePointsOnLine(-1, 4, 0, -3);
+            var expectedA = new List<(int, int)>()
+            {
+                (0,-3), (0,-2), (0,-1), (0,0), (-1,1), (-1,2), (-1,3), (-1,4)
+            };
+            Assert.Equal(expectedA, actualA);
+
+            var actualB = MockCoordinateSystem.CalculatePointsOnLine(1, -4, -1, 4);
+            var expectedB = new List<(int, int)>()
+            {
+                 (1,-4), (1,-3), (1,-2), (0,-1), (0,0), (0,1), (0,2), (-1,3), (-1,4)
+            };
+            Assert.Equal(expectedB, actualB);
+
+            var actualC = MockCoordinateSystem.CalculatePointsOnLine(-2, -10, 1, -3);
+            var expectedC = new List<(int, int)>()
+            {
+               (-2,-10), (-2,-9), (-1,-8), (-1,-7), (0,-6), (0,-5), (1,-4), (1,-3)
+            };
+            Assert.Equal(expectedC, actualC);
+
+            var actualD = MockCoordinateSystem.CalculatePointsOnLine(1, 5, 0, 0);
+            var expectedD = new List<(int, int)>()
+            {
+                (0,0), (0,1), (0,2), (1,3), (1,4), (1,5)
+            };
+            Assert.Equal(expectedD, actualD);
         }
     }
 }
