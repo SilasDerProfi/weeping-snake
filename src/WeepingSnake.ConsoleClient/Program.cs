b8dd902e342bfe0f09e7ba60a674330557204d0c
SilasDerProfi <silas@schnurr.one>
2021-05-17 18:09:23 +0200

    Make Console-output synchronized

--- src/WeepingSnake.ConsoleClient/Program.cs
+++ src/WeepingSnake.ConsoleClient/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using WeepingSnake.Game.Geometry;
 
 namespace WeepingSnake.ConsoleClient
@@ -8,17 +9,26 @@ namespace WeepingSnake.ConsoleClient
     {
         static void Main(string[] args)
         {
-            var gctrl = new Game.GameController(2, 50);
+            var gctrl = new Game.GameController(1, 50);
 
             var playerA = gctrl.JoinGame();
             playerA.AssignedGame.OnLoopTick += PrintGameState;
+
+            gctrl.DoAction(playerA, Game.Player.PlayerAction.Action.CHANGE_NOTHING);
             gctrl.DoAction(playerA, Game.Player.PlayerAction.Action.SPEED_UP);
+            gctrl.DoAction(playerA, Game.Player.PlayerAction.Action.TURN_LEFT);
+            gctrl.DoAction(playerA, Game.Player.PlayerAction.Action.TURN_RIGHT);
+            gctrl.DoAction(playerA, Game.Player.PlayerAction.Action.SPEED_UP);
+
             Console.ReadLine();
         }
 
+        [MethodImpl(MethodImplOptions.Synchronized)]
         private static void PrintGameState(List<GameDistance> newPaths)
         {
-            Console.WriteLine($"New Gamestate with {newPaths.Count} new Paths");
+            Console.WriteLine($"New Gamestate with {newPaths.Count} new Paths:");
+            newPaths.ForEach(p => Console.WriteLine($"- ({(int)p.StartX};{(int)p.StartY}) - ({(int)p.EndX};{(int)p.EndY})"));
+            
         }
     }
 }
