8054a79d689c72bf2e69a8a079e4fae217affcf2
SilasDerProfi <silas@schnurr.one>
2021-05-17 19:58:01 +0200

    fix vector increase & decrease func

--- src/WeepingSnake.Game/Utility/Extensions/Vector2Extensions.cs
+++ src/WeepingSnake.Game/Utility/Extensions/Vector2Extensions.cs
@@ -17,9 +17,21 @@ namespace WeepingSnake.Game.Utility.Extensions
         public static Vector2 RotateRight(this Vector2 v, int degrees) => RotateLeft(v, -degrees);
 
 #warning TODO: do not use the 1 hardcoded, but use a const like "speed step"
-        public static Vector2 Increase(this Vector2 v) => new(v.X + 1, v.Y + 1);
+        public static Vector2 Increase(this Vector2 v)
+        {
+            var length = v.Length();
+            var desiredLength = length + 1;
+            var unitvector = new Vector2(v.X / length, v.Y / length);
+            return new Vector2(unitvector.X * desiredLength, unitvector.Y * desiredLength);
+        }
 
-        public static Vector2 Decrease(this Vector2 v) => new(v.X - 1, v.Y - 1);
+        public static Vector2 Decrease(this Vector2 v)
+        {
+            var length = v.Length();
+            var desiredLength = length - 1;
+            var unitvector = new Vector2(v.X / length, v.Y / length);
+            return new Vector2(unitvector.X * desiredLength, unitvector.Y * desiredLength);
+        }
 
     }
 }
