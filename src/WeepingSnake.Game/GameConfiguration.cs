b3d91f21304a3405e0c13cd74f3e09296d3f9ef7
Silas Schnurr <git@schnurr.one>
2021-05-19 10:10:42 +0200

    Change GameConstants to readonly (because maybe they change at runtime....)

--- src/WeepingSnake.Game/GameConfiguration.cs
+++ src/WeepingSnake.Game/GameConfiguration.cs
@@ -8,11 +8,36 @@ namespace WeepingSnake.Game
 {
     public static class GameConfiguration
     {
-        public const int DefaultDistance = 1;
-        public const int MinimumRotationAngle = 90;
-        public const int MinSpeedIncrement = 1;
+        public static int DefaultDistance
+        {
+            get
+            {
+                return 1;
+            }
+        }
 
-        public const int RoundDuration = 1000;
+        public static int MinimumRotationAngle
+        {
+            get
+            {
+                return 90;
+            }
+        }
+        public static int MinSpeedIncrement
+        {
+            get
+            {
+                return 1;
+            }
+        }
+
+        public static int RoundDuration
+        {
+            get
+            {
+                return 1000;
+            }
+        }
 
     }
 }
