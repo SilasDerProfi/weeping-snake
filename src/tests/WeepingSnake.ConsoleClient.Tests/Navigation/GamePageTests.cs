dd2298e4b866489ce17c2bcf6937820c65b7bda8
Silas Schnurr <git@schnurr.one>
2021-05-24 10:18:33 +0200

    Add GamePageTests

---
+++ src/tests/WeepingSnake.ConsoleClient.Tests/Navigation/GamePageTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeepingSnake.ConsoleClient.Navigation;
+using Xunit;
+
+namespace WeepingSnake.ConsoleClient.Tests.Navigation
+{
+    public class GamePageTests
+    {
+        [Fact]
+        public void TestPointsInRectangle()
+        {
+            var actualA = GamePage.PointsInRectangle(-2, 0, 2, 1).OrderBy(p => p.Item1).ThenBy(p => p.Item2).ToList();
+            var expectedA = new List<(int, int)>()
+            {
+                (-2,0),(-2,1), (-1,0), (-1,1), (0,0), (0,1), (1,0), (1,1), (2,0), (2,1)
+            };
+            Assert.Equal(expectedA, actualA);
+        }
+    }
+}
