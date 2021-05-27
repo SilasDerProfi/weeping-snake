72962a0eec432195a20e72a94ae313e5537999e9
Silas Schnurr <git@schnurr.one>
2021-05-27 14:39:04 +0200

    Vector2 duplicated Code refactors

--- src/WeepingSnake.Game/Utility/Extensions/Vector2Extensions.cs
+++ src/WeepingSnake.Game/Utility/Extensions/Vector2Extensions.cs
@@ -5,47 +5,55 @@ namespace WeepingSnake.Game.Utility.Extensions
 {
     public static class Vector2Extensions
     {
-        public static Vector2 RotateLeft(this Vector2 v, int degrees)
+        public static Vector2 RotateLeft(this Vector2 vector, int degrees)
         {
             var radians = Math.PI / 180 * degrees;
 
             return new Vector2(
-                (float)(v.X * Math.Cos(radians) - v.Y * Math.Sin(radians)),
-                (float)(v.X * Math.Sin(radians) + v.Y * Math.Cos(radians)));
+                (float)(vector.X * Math.Cos(radians) - vector.Y * Math.Sin(radians)),
+                (float)(vector.X * Math.Sin(radians) + vector.Y * Math.Cos(radians)));
         }
 
-        public static Vector2 RotateRight(this Vector2 v, int degrees) => RotateLeft(v, -degrees);
+        public static Vector2 RotateRight(this Vector2 vector, int degrees) => RotateLeft(vector, -degrees);
 
-        public static Vector2 Increase(this Vector2 v)
+        public static Vector2 Increase(this Vector2 vector)
         {
-            var length = v.Length();
-            var desiredLength = length + GameConfiguration.MinSpeedIncrement;
-
-            var unitvector = new Vector2(v.X / length, v.Y / length);
-            return new Vector2(unitvector.X * desiredLength, unitvector.Y * desiredLength);
+            return Add(vector, GameConfiguration.MinSpeedIncrement);
         }
 
-        public static Vector2 Decrease(this Vector2 v)
+        public static Vector2 Decrease(this Vector2 vector)
         {
-            var length = v.Length();
-            var desiredLength = length - GameConfiguration.MinSpeedIncrement;
+            return Add(vector, -GameConfiguration.MinSpeedIncrement);
+        }
+
+        public static Vector2 Add(this Vector2 vector, int lengthSummand)
+        {
+            var length = vector.Length();
+            var desiredLength = length + lengthSummand;
             if (desiredLength <= 0)
                 desiredLength = GameConfiguration.DefaultDistance;
 
-            var unitvector = new Vector2(v.X / length, v.Y / length);
-            return new Vector2(unitvector.X * desiredLength, unitvector.Y * desiredLength);
+            return vector.ChangeLengthTo(desiredLength);
         }
+
 
         public static Vector2 UnitVector(this Vector2 v)
         {
-            var length = v.Length();
-            return new Vector2(v.X / length, v.Y / length);
+            const int unitVectorLength = 1;
+            return v.ChangeLengthTo(unitVectorLength);
         }
 
-        public static Vector2 DefaultDistanceVector(this Vector2 v)
+        public static Vector2 DefaultDistanceVector(this Vector2 vector)
         {
-            var unitVector = v.UnitVector();
-            return new Vector2(unitVector.X * GameConfiguration.DefaultDistance, unitVector.Y * GameConfiguration.DefaultDistance);
+            return vector.ChangeLengthTo(GameConfiguration.DefaultDistance);
+        }
+
+        public static Vector2 ChangeLengthTo(this Vector2 vector, float length)
+        {
+            var actualLength = vector.Length();
+            var unitVector = new Vector2(vector.X / actualLength, vector.Y / actualLength);
+
+            return new Vector2(unitVector.X * length, unitVector.Y * length);
         }
 
     }
